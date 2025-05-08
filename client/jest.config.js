8f321850c2ba0116e0dda83fb8a48544515dafc2
NithinS74 <nithindude74@gmail.com>
2025-05-08 14:29:51 +0530

    with tests

---
+++ client/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "^@/(.*)$": "<rootDir>/src/$1", // Adjust if using Vite aliases
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+};
